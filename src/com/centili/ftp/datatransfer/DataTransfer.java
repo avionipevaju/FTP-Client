94448446bb077cd8454945a939c0cc1bb8b51819
avionipevaju <antic.nikola@outlook.com>
2017-09-14 14:11:06 +0200

    Initial commit

---
+++ src/com/centili/ftp/datatransfer/DataTransfer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.centili.ftp.datatransfer;
+
+/**
+ * @author antic
+ *
+ */
+public class DataTransfer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/centili/ftp/protocolinterpreter/ProtocolInterpreter.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.centili.ftp.protocolinterpreter;
+
+/**
+ * @author antic
+ *
+ */
+public class ProtocolInterpreter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
